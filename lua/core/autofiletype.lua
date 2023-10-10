7cd595ce1c4a438692c1331babfed7b813b587f2
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-10-10 20:01:58 +0700

    fix: remove unused pattern

--- lua/core/autofiletype.lua
+++ lua/core/autofiletype.lua
@@ -1,7 +1,7 @@
 local autocmd = vim.api.nvim_create_autocmd
 
 autocmd({ "BufEnter" }, {
-	pattern = { "*.json", "*.masonrc" },
+	pattern = { "*.json" },
 	command = "set filetype=jsonc",
 	desc = "Enable syntax for .json files",
 })
