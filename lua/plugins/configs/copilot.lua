e17fc357f7e3b13f5e641ec2fa3187e16a144719
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-10-11 06:36:30 +0700

    chore: remove unused comment

--- lua/plugins/configs/copilot.lua
+++ lua/plugins/configs/copilot.lua
@@ -42,13 +42,6 @@ local options = {
 		svn = false,
 		cvs = false,
 		sh = true,
-		-- sh = function()
-		-- 	if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
-		-- 		-- disable for .env files
-		-- 		return false
-		-- 	end
-		-- 	return true
-		-- end,
 	},
 	copilot_node_command = "node", -- Node.js version must be > 16.x
 	server_opts_overrides = {},
