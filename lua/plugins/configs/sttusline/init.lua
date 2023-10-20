38e778238e2cec97e5f1d9410a9434f77cc6e36c
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-10-20 13:26:52 +0700

    fix: use default option sttusline

--- lua/plugins/configs/sttusline/init.lua
+++ lua/plugins/configs/sttusline/init.lua
@@ -2,15 +2,15 @@ local options = {
 	-- 0 | 1 | 2 | 3
 	-- recommended: 3
 	-- laststatus = 3,
-	disabled = {
-		-- filetypes = {
-		-- 	-- "NvimTree",
-		-- 	-- "lazy",
-		-- },
-		buftypes = {
-			"terminal",
-		},
-	},
+	-- disabled = {
+	-- filetypes = {
+	-- 	-- "NvimTree",
+	-- 	-- "lazy",
+	-- },
+	-- 	buftypes = {
+	-- 		"terminal",
+	-- 	},
+	-- },
 	-- components = {
 	-- 	"mode",
 	-- 	"filename",
