4a14508bbc049572f4996918981304085f47ad94
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-11-12 21:06:46 +0700

    dd node bin path to neovim env

---
+++ lua/core/environment.lua
@@ -0,0 +1,17 @@
+local fn = vim.fn
+
+local function get_node_bin()
+	local node_dir = fn.expand("$HOME") .. "/.nvm/versions/node"
+	if fn.isdirectory(node_dir) == 0 then return "" end
+	local node_version = fn.system("ls -v " .. node_dir .. " | tail -n 1")
+	return fn.empty(node_version) == 1 and "" or node_dir .. "/" .. node_version .. "/bin/node"
+end
+
+-- add more environment paths here
+local environment_paths = {
+	fn.stdpath("data") .. "/mason/bin", -- add binaries installed by mason.nvim to path
+	get_node_bin(), -- add node to path if using nvm to manage node versions
+}
+
+local separator = vim.loop.os_uname().sysname == "Windows_NT" and ";" or ":"
+vim.env.PATH = table.concat(environment_paths, separator) .. separator .. vim.env.PATH
--- lua/plugins/configs/copilot.lua
+++ lua/plugins/configs/copilot.lua
@@ -1,14 +1,3 @@
--- local function get_node_path()
--- 	local node_dir = vim.fn.expand("$HOME") .. "/.nvm/versions/node"
-
--- 	if vim.fn.isdirectory(node_dir) == 0 then return "node" end
-
--- 	-- get the latest version of node
--- 	local node_version = vim.fn.system("ls -v " .. node_dir .. " | tail -n 1")
-
--- 	if vim.fn.empty(node_version) == 1 then return "node" end
--- 	return node_dir .. "/" .. node_version .. "/bin/node"
--- end
 local options = {
 	panel = {
 		enabled = true,
@@ -50,8 +39,8 @@ local options = {
 		sh = true,
 	},
 	-- change this to your node binary path
-	copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v21.0.0/bin/node",
-	-- copilot_node_command = "node",
+	-- copilot_node_command = vim.fn.expand("$HOME") .. "/.nvm/versions/node/v21.0.0/bin/node",
+	copilot_node_command = "node",
 	server_opts_overrides = {},
 }
 
