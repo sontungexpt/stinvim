9e46b4686d6943a319d423a2be016395c4c54448
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-11-12 21:37:21 +0700

    fix: add nvm env for window user

--- lua/core/environment.lua
+++ lua/core/environment.lua
@@ -1,11 +1,19 @@
 local fn = vim.fn
+local is_windows = vim.loop.os_uname().sysname == "Windows_NT"
 
 local function get_node_bin()
 	local nvm_dir = fn.expand("$NVM_DIR")
-	local node_dir = fn.empty(nvm_dir) == 1 and fn.expand("$HOME") .. "/.nvm/versions/node"
-		or nvm_dir .. "/versions/node"
+	local node_dir = fn.empty(nvm_dir) == 0 and nvm_dir .. "/versions/node"
+		or is_windows and fn.expand("$HOME") .. "/AppData/Local/nvm/versions/node"
+		or fn.expand("$HOME") .. "/.nvm/versions/node"
+		or ""
+
 	if fn.isdirectory(node_dir) == 0 then return "" end
-	local node_version = fn.system("ls -v " .. node_dir .. " | tail -n 1")
+
+	local node_version = is_windows
+			and fn.system("dir /b /ad /o-n " .. node_dir .. ' | findstr /r /b /c:"v"'):gsub("\n", "")
+		or fn.system("ls -v " .. node_dir .. " | tail -n 1"):gsub("\n", "")
+
 	return fn.empty(node_version) == 1 and "" or node_dir .. "/" .. node_version .. "/bin/node"
 end
 
@@ -15,5 +23,5 @@ local environment_paths = {
 	get_node_bin(), -- add node to path if using nvm to manage node versions
 }
 
-local separator = vim.loop.os_uname().sysname == "Windows_NT" and ";" or ":"
+local separator = is_windows and ";" or ":"
 vim.env.PATH = table.concat(environment_paths, separator) .. separator .. vim.env.PATH
