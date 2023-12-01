a648d9a453d16c075abdffc3091dfa61aed46f94
Tran Vo Son Tung <sontungexpt@gmail.com>
2023-12-01 19:43:58 +0700

    feat: add command to open OpenApiDocuments

--- lua/user/autocmd.lua
+++ lua/user/autocmd.lua
@@ -16,3 +16,23 @@ autocmd({ "BufWritePost" }, {
 	pattern = { fn.expand("$HOME") .. "/scripts/stilux/systems/*" },
 	callback = require("user.utils").compile_stilux_srcipt_file,
 })
+
+autocmd("FileType", {
+	group = augroup("OpenApiDoc", { clear = true }),
+	pattern = { "java" },
+	callback = function(args)
+		api.nvim_create_user_command("OpenApiDoc", function()
+			fn.jobstart("xdg-open http://localhost:8080/swagger-ui/index.html", {
+				detach = true,
+				on_exit = function(_, code, _)
+					if code ~= 0 then
+						require("utils.notify").error("Failed to open swagger-ui")
+					else
+						require("utils.notify").info("Opening http://localhost:8080/swagger-ui/index.html")
+					end
+				end,
+			})
+		end, { nargs = 0 })
+		require("utils.mapper").map("n", "<leader>po", ":OpenApiDoc<CR>", { buffer = args.buf })
+	end,
+})
