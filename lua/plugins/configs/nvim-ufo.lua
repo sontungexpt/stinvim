eb9c1d949f52c93c5c4fa784d53ecef158502cca
Tran Vo Son Tung <sontungexpt@gmail.com>
2024-03-17 16:07:08 +0700

    fix: update nvim-ufo config

--- lua/plugins/configs/nvim-ufo.lua
+++ lua/plugins/configs/nvim-ufo.lua
@@ -1,29 +1,3 @@
-local handler = function(virtText, lnum, endLnum, width, truncate)
-	local suffix = ("  %d "):format(endLnum - lnum)
-	local sufWidth = vim.fn.strdisplaywidth(suffix)
-	local targetWidth = width - sufWidth
-	local newVirtText = {}
-	local curWidth = 0
-	for _, chunk in ipairs(virtText) do
-		local chunkText = chunk[1]
-		local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-
-		if targetWidth > curWidth + chunkWidth then
-			-- The chunk fits within the target width, so add it to the new table.
-			table.insert(newVirtText, chunk)
-			curWidth = curWidth + chunkWidth
-		else
-			chunkText = truncate(chunkText, targetWidth - curWidth)
-			table.insert(newVirtText, { chunkText, chunk[2] })
-			curWidth = curWidth + vim.fn.strdisplaywidth(chunkText)
-			if curWidth < targetWidth then string.insert(suffix, " ", suffix:len() + 1) end
-			break
-		end
-	end
-	table.insert(newVirtText, { suffix, "MoreMsg" })
-	return newVirtText
-end
-
 local options = {
 	open_fold_hl_timeout = 100,
 	-- close_fold_kinds_for_ft = {
@@ -62,7 +36,31 @@ local options = {
 					:catch(function(err) return handleFallbackException(bufnr, err, "indent") end)
 			end
 	end,
-	fold_virt_text_handler = handler,
+	fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+		local suffix = ("  %d "):format(endLnum - lnum)
+		local sufWidth = vim.fn.strdisplaywidth(suffix)
+		local targetWidth = width - sufWidth
+		local newVirtText = {}
+		local curWidth = 0
+		for _, chunk in ipairs(virtText) do
+			local chunkText = chunk[1]
+			local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+
+			if targetWidth > curWidth + chunkWidth then
+				-- The chunk fits within the target width, so add it to the new table.
+				table.insert(newVirtText, chunk)
+				curWidth = curWidth + chunkWidth
+			else
+				chunkText = truncate(chunkText, targetWidth - curWidth)
+				table.insert(newVirtText, { chunkText, chunk[2] })
+				curWidth = curWidth + vim.fn.strdisplaywidth(chunkText)
+				if curWidth < targetWidth then string.insert(suffix, " ", suffix:len() + 1) end
+				break
+			end
+		end
+		table.insert(newVirtText, { suffix, "MoreMsg" })
+		return newVirtText
+	end,
 }
 
 return options
