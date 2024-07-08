e5d42f2214db36eb7ddde33a335a88703b124b8e
Tran Vo Son Tung <sontungexpt@gmail.com>
2024-07-08 15:21:27 +0700

    config lsp.handlers one time

--- lua/plugins/configs/lsp/default.lua
+++ lua/plugins/configs/lsp/default.lua
@@ -9,23 +9,22 @@ local signs_hl = {
 	"DiagnosticSignInfo",
 }
 
-vim.diagnostic.config {
-	signs = {
-		text = require("ui.icons").DiagnosticSign,
-		numhl = signs_hl,
-		texthl = signs_hl,
-	},
-	underline = true,
-	severity_sort = true,
-	update_in_insert = true,
-	virtual_text = false,
-	float = {
-		source = "if_many",
-	},
-}
+if not vim.g.stinvim_general_lsp_config_loaded then
+	vim.diagnostic.config {
+		signs = {
+			text = require("ui.icons").DiagnosticSign,
+			numhl = signs_hl,
+			texthl = signs_hl,
+		},
+		underline = true,
+		severity_sort = true,
+		update_in_insert = true,
+		virtual_text = false,
+		float = {
+			source = "if_many",
+		},
+	}
 
-M.on_attach = function(client, bufnr)
-	require("better-diagnostic-virtual-text").setup_buf(bufnr, nil)
 	lsp.handlers["textDocument/signatureHelp"] = lsp.with(lsp.handlers.signature_help, {
 		border = "single",
 		focusable = false,
@@ -33,41 +32,45 @@ M.on_attach = function(client, bufnr)
 	})
 
 	lsp.handlers["textDocument/hover"] = lsp.with(lsp.handlers.hover, { border = "single" })
-end
 
-local capabilities = lsp.protocol.make_client_capabilities()
-	or require("lspconfig").util.default_config.capabilities
-	or require("cmp_nvim_lsp").default_capabilities()
+	M.on_attach = function(client, bufnr) require("better-diagnostic-virtual-text").setup_buf(bufnr, nil) end
 
-capabilities.textDocument.completion.completionItem = {
-	snippetSupport = true,
-	preselectSupport = true,
-	insertReplaceSupport = true,
-	labelDetailsSupport = true,
-	deprecatedSupport = true,
-	commitCharactersSupport = true,
-	documentationFormat = {
-		"markdown",
-		"plaintext",
-	},
-	tagSupport = {
-		valueSet = { 1 },
-	},
-	resolveSupport = {
-		properties = {
-			"documentation",
-			"detail",
-			"additionalTextEdits",
+	local capabilities = lsp.protocol.make_client_capabilities()
+		or require("lspconfig").util.default_config.capabilities
+		or require("cmp_nvim_lsp").default_capabilities()
+
+	capabilities.textDocument.completion.completionItem = {
+		snippetSupport = true,
+		preselectSupport = true,
+		insertReplaceSupport = true,
+		labelDetailsSupport = true,
+		deprecatedSupport = true,
+		commitCharactersSupport = true,
+		documentationFormat = {
+			"markdown",
+			"plaintext",
 		},
-	},
-}
+		tagSupport = {
+			valueSet = { 1 },
+		},
+		resolveSupport = {
+			properties = {
+				"documentation",
+				"detail",
+				"additionalTextEdits",
+			},
+		},
+	}
 
--- for nvim-ufo
-capabilities.textDocument.foldingRange = {
-	dynamicRegistration = false,
-	lineFoldingOnly = true,
-}
+	-- for nvim-ufo
+	capabilities.textDocument.foldingRange = {
+		dynamicRegistration = false,
+		lineFoldingOnly = true,
+	}
 
-M.capabilities = capabilities
+	M.capabilities = capabilities
+
+	vim.g.stinvim_general_lsp_config_loaded = true
+end
 
 return M
