2e19887d14827383ccd58de33165a28bb8ee0b8c
Tran Vo Son Tung <sontungexpt@gmail.com>
2024-07-08 22:27:03 +0700

    update config

--- lua/plugins/configs/lsp/lspconfig.lua
+++ lua/plugins/configs/lsp/lspconfig.lua
@@ -40,12 +40,6 @@ local lsp_servers = {
 	},
 	{
 		name = "eslint",
-		config = {
-			on_attach = function(client, bufnr)
-				on_attach(client, bufnr)
-				client.resolved_capabilities.document_formatting = true
-			end,
-		},
 	},
 	{
 		name = "svelte",
