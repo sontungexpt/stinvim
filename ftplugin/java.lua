cdd4a52e9831d649d69b7b1c132edc161bf572b7
Tran Vo Son Tung <sontungexpt@gmail.com>
2024-07-20 20:35:35 +0700

    update jdtls config

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,11 +1,10 @@
-local vim = vim
-
 local ok, jdtls = pcall(require, "jdtls")
 if not ok then return end
 
+local vim = vim
 local fn, uv, fs = vim.fn, (vim.uv or vim.loop), vim.fs
-
 local mason_registry_get_package = require("mason-registry").get_package
+
 local jdtls_path = mason_registry_get_package("jdtls"):get_install_path()
 
 local jar_path = fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar", true, true)[1]
@@ -20,16 +19,38 @@ local jtest_path = mason_registry_get_package("java-test"):get_install_path()
 local uname = uv.os_uname().sysname
 uname = uname == "Linux" and "linux" or uname == "Darwin" and "mac" or "win"
 
-local root_dir = fs.dirname(fs.find({ ".git", "pom.xml", "build.gradle" }, { upward = true })[1])
-local project_name = fn.fnamemodify(root_dir, ":p:h:t")
+local root_dir = fs.root(0, { ".git", "pom.xml", "build.gradle" })
+if not root_dir then return end
+
+local project_name = fs.basename(root_dir)
 
 local workspace_dir = fn.stdpath("cache") .. "/site/java/workspace-root/" .. project_name
 if fn.isdirectory(workspace_dir) == 0 then fn.mkdir(workspace_dir, "p") end
 
-local function java()
-	return fn.filereadable("/usr/lib/jvm/java-17-openjdk/bin/java") == 1
-			and "/usr/lib/jvm/java-17-openjdk/bin/java"
-		or fn.exepath("java")
+local function find_java()
+	local java_priority_paths = {
+		"/usr/lib/jvm/java-17-openjdk/bin/java",
+		"/usr/lib/jvm/java-11-openjdk/bin/java",
+		"/usr/lib/jvm/java-8-openjdk/bin/java",
+	}
+	for i, path in ipairs(java_priority_paths) do
+		if fn.filereadable(path) == 1 then return path, {
+			name = i,
+			path = path,
+		} end
+	end
+	local java = fn.exepath("java")
+	if java ~= "" then return java, {
+		name = 1,
+		path = java,
+	} end
+	return nil, nil
+end
+
+local java_path, runtime_paths = find_java()
+if not java_path then
+	require("utils.notify").error("Java not found")
+	return
 end
 
 local extendedClientCapabilities = jdtls.extendedClientCapabilities
@@ -37,7 +58,7 @@ extendedClientCapabilities.resolveAdditionalTextEditsSupport = true
 
 local config = {
 	cmd = {
-		java(),
+		java_path,
 		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
 		"-Dosgi.bundles.defaultStartLevel=4",
 		"-Declipse.product=org.eclipse.jdt.ls.core.product",
@@ -57,16 +78,12 @@ local config = {
 		"-data",
 		workspace_dir,
 	},
-	capabilities = require("plugins.configs.lsp.default").capabilities,
+	capabilities = require("config.lsp.default").capabilities,
 	on_attach = function(client, bufnr)
 		jdtls.setup_dap { hotcodereplace = "auto" }
 
-		require("jdtls.dap").setup_dap_main_class_configs()
 		require("jdtls.setup").add_commands()
-
-		require("plugins.configs.lsp.default").on_attach(client, bufnr)
-
-		require("lspsaga").init_lsp_saga()
+		require("config.lsp.default").on_attach(client, bufnr)
 	end,
 	root_dir = root_dir,
 	settings = {
@@ -95,7 +112,7 @@ local config = {
 			format = {
 				enabled = true,
 				settings = {
-					url = fn.stdpath("config") .. "/lang_servers/intellij-java-google-style.xml",
+					url = fn.stdpath("config") .. "/lang-servers/intellij-java-google-style.xml",
 					profile = "GoogleStyle",
 				},
 			},
@@ -139,28 +156,30 @@ local config = {
 				useBlocks = true,
 			},
 			configuration = {
-				runtimes = {
-					{
-						name = "JavaSE-11",
-						path = "/usr/lib/jvm/java-11-openjdk/",
-					},
-					{
-						name = "JavaSE-17",
-						path = "/usr/lib/jvm/java-17-openjdk/",
-					},
-				},
+				runtimes = runtime_paths,
+				-- runtimes = {
+				-- 	-- {
+				-- 	-- 	name = "JavaSE-11",
+				-- 	-- 	path = "/usr/lib/jvm/java-11-openjdk/",
+				-- 	-- },
+				-- 	{
+				-- 		name = "JavaSE-17",
+				-- 		path = "/usr/lib/jvm/java-17-openjdk/",
+				-- 	},
+				-- },
 			},
 		},
-	},
-	flags = {
-		allow_incremental_sync = true,
-	},
-	init_options = {
-		extendedClientCapabilities = extendedClientCapabilities,
-		bundles = vim.list_extend(
-			fn.glob(jdebug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar", true, true),
-			fn.glob(jtest_path .. "/extension/server/*.jar", true, true)
-		),
+		flags = {
+			debounce_text_changes = 150,
+			allow_incremental_sync = true,
+		},
+		init_options = {
+			extendedClientCapabilities = extendedClientCapabilities,
+			bundles = (jdebug_path ~= "" and jtest_path ~= "" and vim.list_extend(
+				fn.glob(jdebug_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar", true, true),
+				fn.glob(jtest_path .. "/extension/server/*.jar", true, true)
+			)) or nil,
+		},
 	},
 }
 
