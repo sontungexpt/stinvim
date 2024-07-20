3157e397b827a9466187d2941c1358a2f6dd7dab
Tran Vo Son Tung <sontungexpt@gmail.com>
2024-07-20 15:58:54 +0700

    use masonrc only

--- lua/config/mason.lua
+++ lua/config/mason.lua
@@ -1,55 +1,6 @@
 return {
 	-- auto sync the installed packages with ensure_installed when open nvim
 	auto_sync = true,
-
-	-- I recommend you create .masonrc.json file instead using this key to make the command MasonSyncPackages working
-	ensure_installed = {
-		-- lua
-		"lua-language-server",
-		"stylua",
-
-		-- bash
-		"bash-language-server",
-		"shfmt",
-		"shellcheck",
-
-		-- toml
-		"taplo",
-
-		-- zig
-		"zls",
-
-		-- c++
-		"clangd",
-		"clang-format",
-		"cmake-language-server",
-		"codelldb",
-
-		-- python
-		"pyright",
-		"autopep8",
-
-		-- others
-		"codespell",
-
-		-- web-developments
-		-- "prettier",
-		"prettierd",
-		"eslint-lsp",
-		"css-lsp",
-		"html-lsp",
-		"typescript-language-server",
-		"svelte-language-server",
-		"emmet-ls",
-		"json-lsp",
-		"tailwindcss-language-server",
-		"xmlformatter",
-
-		-- java
-		"jdtls",
-		"google-java-format",
-		"java-debug-adapter",
-	},
 	ui = {
 		check_outdated_packages_on_open = true,
 		border = "single", -- Accepts same border values as |nvim_open_win()|.
